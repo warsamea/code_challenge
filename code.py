f9e72ab8e4ac8be9a77d3650582459a02b3e793a
ayanwarsame <warsameayan1@gmail.com>
2022-07-11 15:16:45 +0100

    add comments

--- code.py
+++ code.py
@@ -1,3 +1,6 @@
+# provide code that iterates in multiples of A until X,
+# then in multiples of A + 1 until 2X,
+# then multiples of A + 2 until 3X
 def multiples_func(x,a):
     a1 = a + 1
     x2 = 2 * x
@@ -9,7 +12,8 @@ def multiples_func(x,a):
 
     return a_0, a_1, a_2
 
-# y,z,b = multiples_func(20,2)
-# print(y)
-# print(z)
-# print(b)
+# example x and a below:
+y,z,b = multiples_func(20,2)
+print(y)
+print(z)
+print(b)
